e873435d80aab63cf183ba94763da27affd36208
yanhaixiang <1729628996@qq.com>
2019-04-11 16:56:48 +0800

    提交

---
+++ Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public string UserPhone { get; set; }
+        public int UserMemeber { get; set; }
+        public int UserIntegral { get; set; }
+    }
+}
